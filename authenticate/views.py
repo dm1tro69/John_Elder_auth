4c587999aa14c8012a9d6e3c860426fe55b42350
dm1tro69 <dimolg22@gmail.com>
2019-12-26 19:42:47 +0200

    register

--- authenticate/views.py
+++ authenticate/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, logout, login
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 
 # Create your views here.
 
@@ -28,3 +30,20 @@ def logout_user(request):
     logout(request)
     messages.success(request, ('You Have Been Logged Out.'))
     return redirect('home')
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ('You Have Registered.'))
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    context = {'form': form}
+    return render(request, 'authenticate/register.html', context)
